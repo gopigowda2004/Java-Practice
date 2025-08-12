80968926473853d20cba2de8903ad413fc034447
Gopi C N <gopigowda132@gmail.com>
2025-08-12 23:08:40 +0530

    This is my first day 2 problem

---
+++ src/Swap.java
@@ -0,0 +1,13 @@
+public class Swap {
+ public static void main(String[] args) {
+    int a=12;
+    int b=13;
+    int temp=a;
+    a=b;
+    b=temp;
+    //a=a+b;
+    //b=a-b;
+    //a=a-b;
+    System.out.println(b+"  "+a);
+ }
+}
