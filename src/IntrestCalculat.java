962a45b3a00d44f82d11a55ec961f32acbd035ac
Gopi C N <gopigowda132@gmail.com>
2025-08-12 23:24:03 +0530

    Java problems

---
+++ src/IntrestCalculat.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class IntrestCalculat {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter Principle:");
+    int p=sc.nextInt();
+    System.out.println("Enter Rate:");
+    int R=sc.nextInt();
+    System.out.println("Enter Time:");
+    int T=sc.nextInt();
+
+    int SI=p*R*T;
+    float f=SI/100;
+    System.out.println(f);
+
+  }
+}
