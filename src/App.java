5d3b6a1baf89cf3394033ff97582b2ffe27902bc
Gopi C N <gopigowda132@gmail.com>
2025-08-12 23:00:30 +0530

    This is my first day

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World this is my first program!");
+        System.out.println("Date:12-08-2025");
+
+    }
+}
