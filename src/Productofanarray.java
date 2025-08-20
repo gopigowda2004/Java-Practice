8f3a9b00b0b8b1ba62b3a2145517660cc70764da
Gopi C N <gopigowda132@gmail.com>
2025-08-20 21:33:58 +0530

    java problems

--- src/Productofanarray.java
+++ src/Productofanarray.java
@@ -1,3 +1,17 @@
 public class Productofanarray {
+   public static void main(String[] args) {
+    int strs;
+    if(strs==null || strs.length==0) return "";
 
+    String Prefix=strs[0];
+     
+     for(int i=1;i<strs.length;i++){
+        while(!strs.startswith(Prefix)){
+            Prefix=Prefix.substring(0, Prefix.length()-1);
+
+            if(Prefix.isEqual()) return ""; 
+        }
+     }
+     return Prefix;
+   }
 }
