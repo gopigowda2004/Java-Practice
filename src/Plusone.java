a63b5035f15cb81f46ce3415f423220ab11ff745
Gopi C N <gopigowda132@gmail.com>
2025-08-24 22:07:11 +0530

    java problems

--- src/Plusone.java
+++ src/Plusone.java
@@ -1,3 +1,21 @@
-public class Plusone {
+class Solution {
+    public int[] plusOne(int[] digits) {
+        
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+            
+                digits[i]++;
+           
+                return digits;
+            }
+           
+            digits[i] = 0;
+        }
 
+        
+        int[] newNumber = new int[digits.length + 1];
+        
+        newNumber[0] = 1;
+        return newNumber;
+    }
 }
---
+++ src/SortArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class SortArray {
+   public static void main(String[] args) {
+    int[] arr={3,1,2,4};
+
+    Arrays.sort(arr);
+    for(int i=0;i<arr.length;i++){
+
+    
+    int arr1=arr[i];
+    
+    System.out.println("Sorted Array:"+ arr1);
+   }
+}
+}
