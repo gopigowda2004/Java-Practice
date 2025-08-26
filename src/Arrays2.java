94f70d186bc1edee5184d294a7912f7b1b211280
Gopi C N <gopigowda132@gmail.com>
2025-08-26 21:23:19 +0530

    Results

---
+++ src/Arrays2.java
@@ -0,0 +1,12 @@
+public class Arrays2 {
+   public static void main(String[] args) {
+    int[] arr={1,2,3,4,5,6,7};
+    System.out.println(arr[0]+" "+ arr[arr.length-1]+ " "+arr.length);
+      for(int i=0;i<arr.length;i++){
+        if(arr[i]%2==0){
+         System.out.println(arr[i]+ "Even numbers:");
+        }
+      }
+     }
+
+}
---
+++ src/ReverseArray.java
@@ -0,0 +1,5 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr={10,20,30,40,50};
+    }
+}
